8633324c019008719e072e85b1e8cdec783c0f6d
nicolas graham <nicolasgraham@seattleacademy.org>
2020-02-05 09:16:18 -0800

    new stuff

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world);
+const express = require('express')
+const app = express()
+const port = 1512
+let counter = 0
+
+app.get('/', (req, res) => res.send('Hello Nicolas!'))
+
+app.get('/counter', function (req, res) { 
+	couter = counter + 1;
+  res.send('counter ' + counter);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
